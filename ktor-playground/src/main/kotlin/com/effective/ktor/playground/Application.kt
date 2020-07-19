cfe2ba8a03980989fde27fbc5918441019d81d34
Vladimir Zatsepin <zatsepin.vl@gmail.com>
2020-07-19 12:30:57 +0300

    Update repo

---
+++ ktor-playground/src/main/kotlin/com/effective/ktor/playground/Application.kt
@@ -0,0 +1,53 @@
+package com.effective.ktor.playground
+
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.websocket.*
+import io.ktor.http.cio.websocket.*
+import java.time.*
+import com.fasterxml.jackson.databind.*
+import io.ktor.jackson.*
+import io.ktor.features.*
+import io.ktor.server.netty.EngineMain
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+@Suppress("unused")
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(WebSockets) {
+        pingPeriod = Duration.ofSeconds(15)
+        timeout = Duration.ofSeconds(15)
+        maxFrameSize = Long.MAX_VALUE
+        masking = false
+    }
+
+    install(ContentNegotiation) {
+        jackson {
+            enable(SerializationFeature.INDENT_OUTPUT)
+        }
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/json/jackson") {
+            call.respond(mapOf("hello" to "world"))
+        }
+
+        webSocket("/myws/echo") {
+            send(Frame.Text("Hi from server"))
+            while (true) {
+                val frame = incoming.receive()
+                if (frame is Frame.Text) {
+                    send(Frame.Text("Client said: " + frame.readText()))
+                }
+            }
+        }
+    }
+}
+
---
+++ quarkus-playground/src/native-test/kotlin/com/example/NativeExampleResourceIT.kt
@@ -0,0 +1,6 @@
+package com.example
+
+import io.quarkus.test.junit.NativeImageTest
+
+@NativeImageTest
+class NativeExampleResourceIT : ExampleResourceTest()
